ca9fc7a2cc075ed6acf79e69d8614e99e23f0cc3
Shambonik <shambonik@gmail.com>
2021-03-17 10:46:06 +0300

    Plugin

---
+++ AndroidProject/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    private const string CLASS_NAME = "com.example.mylibrary2.MyClass";
+    // private const string UnityActivity = "com.unity3d.player.UnityPlayerActivity";
+    private static AndroidJavaClass JavaClass;
+
+    public void Start()
+    {
+        JavaClass = new AndroidJavaClass(CLASS_NAME);
+    }
+
+    public void OnClick()
+    {
+        if(JavaClass!=null)
+        {
+            FindObjectOfType<Text>().text = "here1";
+            AndroidJavaClass ajc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject ajo = ajc.GetStatic<AndroidJavaObject>("currentActivity");
+            FindObjectOfType<Text>().text = "here2";
+            JavaClass.CallStatic("launchActivity", ajo);
+            FindObjectOfType<Text>().text = "here3";
+        }
+        else
+        {
+            FindObjectOfType<Text>().text = "here";
+        }
+    }
+}
